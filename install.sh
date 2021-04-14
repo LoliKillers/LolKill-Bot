d5c63c19145d86a0c0f88e32f2ccfc135daa62b8
002 <80396688+Zer0-Tw0@users.noreply.github.com>
2021-04-14 23:32:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ apt-get install libwebp
 apt-get install mc
 apt-get install ffmpeg
 apt-get install wget
-apt-get install tesseract-ocr
+apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 
 npm install
